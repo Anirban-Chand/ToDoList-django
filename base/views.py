f51b7bc2e2312a67304b46df949760d4faa4777e
Anirban-Chand <anirbanchand54@gmail.com>
2022-03-02 16:01:20 +0530

    User Registration Done

--- base/views.py
+++ base/views.py
@@ -1,21 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout
 
 from .models import Task
 
 
 class CustomLoginView(LoginView):
-    template_name = 'base\login.html'
+    template_name = 'base/login.html'
     fields = '__all__'
     redirect_authenticated_user = True
 
     def get_success_url(self):
         return reverse_lazy('tasks')
+
+
+class RegisterPage(FormView):
+    template_name = 'base/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(RegisterPage, self).get(*args, *kwargs)
+
 
 
 # ListView looks for a template with name -> <model_name>_list
@@ -29,10 +50,9 @@ class TaskList(LoginRequiredMixin, ListView):
     # this method provides sand-boxing among all the users. so that a user can access it's own data only and not other's
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["tasks"] = context["tasks"].filter(user = self.request.user)
+        context["tasks"] = context["tasks"].filter(user=self.request.user)
         context["count"] = context["tasks"].filter(complete=False).count()
         return context
-    
 
 
 # DetailView looks for a template with name -> <model_name>_detail
